9e08ab1f8bd6a6c1dc9149e300de79b0d98b5a41
SnowyDuck <37960331+SnowyDuck@users.noreply.github.com>
2019-01-02 17:20:46 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const botcfg = require("./botcfg.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Cant find command");
+    return;
+  }
+
+
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`Command ${f} has loaded`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity("SnowyDuck Tapping Kids", {type: "WATCHING"});
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botcfg.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+});
+
+bot.login(botcfg.token);
